9775fea8e7f85eb3f7d27e7f6f4b6ad44d2b9e8d
Navin Yadav <nry.neo@gmail.com>
2026-05-09 16:46:34 +0530

    initial commit

---
+++ e2e/google.spec.js
@@ -0,0 +1,29 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+    await page.goto('https://www.google.com');
+
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle('Google');
+
+    const title = await page.title();
+    console.log(`Page title: ${title}`);
+
+    const url = page.url();
+    console.log(`Page URL: ${url}`);
+
+    const text = await page.locator("//input[@role='button']").last().getAttribute('value');
+    console.log(`Extracted text: ${text}`);
+
+});
+
+// test('get started link', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
+
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Get started' }).click();
+
+//   // Expects page to have a heading with the name of Installation.
+//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+// });
